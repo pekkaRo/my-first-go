8f5b492eda3360775bbb1d3cb3c0c1f1224a84e8
pekkaRo <roytta@gmail.com>
2022-05-03 22:36:39 +0300

    add french

--- hello.go
+++ hello.go
@@ -2,15 +2,26 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
+const frechHelloPrefix = "Bonjour, "
+const spanisHelloPrefix = "Hola, "
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+	if language == spanish {
+		return spanisHelloPrefix + name
+	}
+	if language == french {
+		return frechHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
