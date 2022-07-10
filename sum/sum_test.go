cd4b4347eca7883222899dfc63388c531ff87966
Pekka Röyttä <pekka.roytta@siqbasketball.com>
2022-07-10 11:05:38 +0300

    make sum use slices

--- sum/sum_test.go
+++ sum/sum_test.go
@@ -4,12 +4,16 @@ import "testing"
 
 func TestSum(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+	
 
-	got := Sum(numbers)
-	want := 15
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6}
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		got := Sum(numbers)
+		want := 21
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 }
